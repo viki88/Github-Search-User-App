fb6456c85b5dc594ce5e2e502feba7068953e8ca
macbookairm1 <vikiandrianto88@gmail.com>
2025-02-21 09:20:33 +0700

    fix: update compile SDK to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.vikination.githubsearchuserapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.vikination.githubsearchuserapp"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
